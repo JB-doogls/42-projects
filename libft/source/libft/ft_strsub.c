59430905006781ff2576a520671aae99025ef498
Edwyle Doll <edoll@ox-p5.21-school.ru>
2019-09-23 21:20:50 +0300

    upd to strsub

--- libft/source/libft/ft_strsub.c
+++ libft/source/libft/ft_strsub.c
@@ -1,33 +1,34 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*   ft_strsub.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: edoll <edoll@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/09/10 20:28:41 by edoll             #+#    #+#             */
-/*   Updated: 2019/09/23 21:19:28 by edoll            ###   ########.fr       */
+/*   Created: 2019/09/23 20:52:58 by edoll             #+#    #+#             */
+/*   Updated: 2019/09/23 21:19:24 by edoll            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strdup(const char *src)
+char	*ft_strsub(char const *s, unsigned int start, size_t len)
 {
-	char	*ptr;
-	size_t	len;
-
-	len = 0;
-	while (src[len])
-		len++;
-	if (!(ptr = (char *)malloc(sizeof(*ptr) * (len + 1))))
-		return (NULL);
-	len = 0;
-	while (src[len])
+	if (s) // && s[start] && (ft_strlen(s) == len))
 	{
-		ptr[len] = src[len];
-		len++;
+		char *sub;
+		size_t ct;
+
+		ct = 0;
+		if (!(sub = (char *)malloc(sizeof(char) * (len + 1))))
+		{
+			free(sub);
+			return (NULL);
+		}
+		while (s[start] && len--)
+			sub[ct++] = s[start++];
+		sub[ct] = '\0';
+		return (sub);
 	}
-	ptr[len] = '\0';
-	return (ptr);
+	return (NULL);
 }
