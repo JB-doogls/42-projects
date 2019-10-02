49382fc073e1655a7a0ec33e796834c64da3bb3c
J.B.Doogls <jbdoogls@edoll.21vv>
2019-10-02 13:59:36 +0300

    "read multiple fd!"

--- PROJECTS/gnl/main.c
+++ PROJECTS/gnl/main.c
@@ -4,20 +4,29 @@
 
 int main(int ac, char **av)
 {
-	(void)ac;
-	char *line = NULL;
-	int fd = open(av[1], O_RDONLY);
-
-	int ret;
-	while ((ret = get_next_line(fd, &line)))
+//	(void)ac;
+	int i = 1;
+	int k = ac;
+	
+	while (k > 1)
 	{
-		if (ret == -1)
+		char *line = NULL;
+		int fd = open(av[i], O_RDONLY);
+
+		int ret;
+		while ((ret = get_next_line(fd, &line)))
 		{
-			printf("error main");
-		//	return (0);
+			if (ret == -1)
+			{
+				printf("error main");
+			//	return (0);
+			}
+			printf("%s\n", line);
 		}
-		printf("%s\n", line);
-	}
+		close (fd);
+		k--;
+		i++;
+		}
 }
 
 // 	fd = open(av[2], O_RDONLY);
