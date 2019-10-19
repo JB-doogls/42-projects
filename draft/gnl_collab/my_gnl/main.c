ce2f76face29dc33d33c0b5279a81efc2ce133df
J.B.Doogls <jbdoogls@edoll.21vv>
2019-10-19 04:13:08 +0300

    implementation with comments

---
+++ draft/gnl_collab/my_gnl/main.c
@@ -0,0 +1,87 @@
+
+#include "get_next_line.h"
+#include <stdio.h>
+#include <inttypes.h>
+
+int main(int argc, char **argv)
+{
+	int fd;
+	int ret;
+	int line_count = 0;
+	char *buff;
+	int	i = 1;
+	int ac = 0;
+
+	if ((ac = argc) > 1)
+	{
+		while (ac-- > 1)
+		{
+		fd = open(argv[i], O_RDONLY);
+		while ((ret = get_next_line(fd, &buff)) > 0)
+		{
+			printf("[Return: %d] Line #%d: %s\n", ret, ++line_count, buff);
+			free(buff);
+		}
+		// printf("[Return: %d] Line #%d: %s\n", ret, ++line_count, buff);
+		free(buff);
+		if (ret == -1)
+			printf("-----------\nError\n");
+		else if (ret == 0)
+			printf("-----------\nEnd of file\n");
+		close(fd);
+		i++;
+		}
+	}
+	if (argc == 1)
+	{
+		while ((ret = get_next_line(0, &buff)) > 0)
+			printf("[Return: %d] Line #%d: %s\n", ret, ++line_count, buff);
+		if (ret == -1)
+			printf("-----------\nError\n");
+		else if (ret == 0)
+			printf("-----------\nEnd of stdin\n");
+		close(fd);
+	}
+	return (0);
+}
+
+
+
+// int main(int ac, char **av)
+// {
+// 	int i = 1;
+	
+// 	char *line = NULL;
+// 	while (ac-- > 1)
+// 	{
+// 		int fd = open(av[i], O_RDONLY);
+// 		int ret;
+// 		while ((ret = get_next_line(fd, &line)))
+// 		{
+// 			if (ret == -1)
+// 			{
+// 				printf("error main");
+// 				return (0);
+// 			}
+// 			printf("{ 1 } ret: %d\n", ret);
+// 			printf("{ 1 }");
+// 			printf("%s\n", line);
+// 			if (line)
+// 			{
+// 				free(line);
+// 				line = NULL;
+// 			}
+// 		}
+// 		printf("{ 2 } ret: %d\n", ret);
+// 		printf("{ 2 }");
+// 		printf("%s\n", line);
+// 		if (line)
+// 		{
+// 			free(line);
+// 			line = NULL;
+// 		}
+// 		close(fd);
+// 		i++;
+// 	}
+// 	return (1);
+// }
